826bd748fee7fe2b49af0aa2fcfeddc2c066e7d4
sonthuol <sonthuolb3@gmail.com>
2024-01-18 02:37:01 +0700

    add input component

---
+++ src/components/InputComponent.tsx
@@ -0,0 +1,91 @@
+import {EyeSlash} from 'iconsax-react-native';
+import React, {ReactNode, useState} from 'react';
+import {
+  KeyboardType,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {appColors} from '../constants/appColors';
+import {globalStyles} from '../styles/globalStyles';
+
+interface Props {
+  value: string;
+  placeholder?: string;
+  affix?: ReactNode;
+  suffix?: ReactNode;
+  isPassword?: boolean;
+  allowClear?: boolean;
+  type?: KeyboardType;
+  onChange: (val: string) => void;
+}
+
+const InputComponent = (props: Props) => {
+  const {
+    value,
+    placeholder,
+    affix,
+    suffix,
+    isPassword,
+    allowClear,
+    type,
+    onChange,
+  } = props;
+
+  const [isShowPassword, setIsShowPassword] = useState(isPassword ?? false);
+
+  return (
+    <View style={[styles.inputContainer]}>
+      {affix ?? affix}
+      <TextInput
+        style={[styles.input, globalStyles.text]}
+        value={value}
+        placeholder={placeholder ?? ''}
+        onChangeText={val => onChange(val)}
+        secureTextEntry={isShowPassword}
+        placeholderTextColor={'#747688'}
+        keyboardType={type ?? 'default'}
+      />
+      {suffix ?? suffix}
+      <TouchableOpacity
+        onPress={
+          isPassword
+            ? () => setIsShowPassword(!isShowPassword)
+            : () => onChange('')
+        }>
+        {isPassword ? (
+          <EyeSlash size={22} color={appColors.gray} />
+        ) : (
+          value.length > 0 &&
+          allowClear && (
+            <AntDesign name="close" size={22} color={appColors.text} />
+          )
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default InputComponent;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: appColors.gray3,
+    width: '100%',
+    minHeight: 56,
+    paddingHorizontal: 15,
+  },
+  input: {
+    padding: 0,
+    margin: 0,
+    flex: 1,
+    paddingHorizontal: 14,
+  },
+});
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {ButtonComponent} from '../../components';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import {globalStyles} from '../../styles/globalStyles';
+import {InputComponent} from '../../components';
+import {Sms} from 'iconsax-react-native';
+import {appColors} from '../../constants/appColors';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+
   return (
-    <View style={[globalStyles.container, {padding: 42}]}>
-      <Text>LoginScreen</Text>
-      {/* <Button title='Login' onPress={async () => await AsyncStorage.setItem("assetToken", "isLoged") } /> */}
-      <ButtonComponent
-        text="LOGIN"
-        type="primary"
-        onPress={() => console.log('LOGIN')}
-        icon={
-          <View>
-            <Text>N</Text>
-          </View>
-        }
+    <View
+      style={[
+        globalStyles.container,
+        {justifyContent: 'center', alignItems: 'center', padding: 20},
+      ]}>
+      <InputComponent
+        value={email}
+        placeholder="Email"
+        onChange={val => setEmail(val)}
+        allowClear
+        affix={<Sms size={22} color={appColors.gray} />}
+        type="email-address"
       />
     </View>
   );
