54d2e39ff7daadc72d38d099741339b3e6894100
sonthuol <sonthuolb3@gmail.com>
2024-01-18 21:45:26 +0700

    create container component

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,21 +1,19 @@
-import React, {useState} from 'react';
-import {View} from 'react-native';
-import {globalStyles} from '../../styles/globalStyles';
-import {InputComponent} from '../../components';
 import {Lock, Sms} from 'iconsax-react-native';
+import React, {useState} from 'react';
+import {ContainerComponent, InputComponent} from '../../components';
 import {appColors} from '../../constants/appColors';
+import {Text, View} from 'react-native';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   return (
-    <View
-      style={[
-        globalStyles.container,
-        {justifyContent: 'center', alignItems: 'center', padding: 20},
-      ]}>
-      <InputComponent
+    <ContainerComponent isImageBackground>
+      <View>
+        <Text>Demo</Text>
+      </View>
+      {/* <InputComponent
         value={email}
         placeholder="Email"
         onChange={val => setEmail(val)}
@@ -30,8 +28,8 @@ const LoginScreen = () => {
         allowClear
         isPassword
         affix={<Lock size={22} color={appColors.gray} />}
-      />
-    </View>
+      /> */}
+    </ContainerComponent>
   );
 };
 
